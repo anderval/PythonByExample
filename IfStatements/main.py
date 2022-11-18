738650ab05c302ffc8b37dc39342b9bd3b43ae15
Anderval <andervalsp@outlook.com>
2022-11-18 14:06:18 -0300

    Second chapter

---
+++ IfStatements/main.py
@@ -0,0 +1,24 @@
+def chlg12():
+    num1 = int(input("Insert first number: "))
+    num2 = int(input("Insert second number: "))
+
+    if num1 > num2:
+        print(num2, num1)
+    else:
+        print(num1, num2)
+
+
+def chlg13():
+    num = int(input("Insert a number under 20: "))
+
+    if num >= 20:
+        print("Too high")
+    else:
+        print("Thank you")
+
+
+def chlg14():
+    num = int(input("Insert a number between 10 and 20: "))
+
+
+chlg13()
