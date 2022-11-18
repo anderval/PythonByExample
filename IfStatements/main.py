ce3789cebac1b641ed8f782c9fb1223643be7f35
Anderval <andervalsp@outlook.com>
2022-11-18 15:49:18 -0300

    Second chapter

--- IfStatements/main.py
+++ IfStatements/main.py
@@ -20,5 +20,71 @@ def chlg13():
 def chlg14():
     num = int(input("Insert a number between 10 and 20: "))
 
+    if num >= 10 and num <= 20:
+        print("Thank you")
+    else:
+        print("Incorrect answer")
 
-chlg13()
+
+def chlg15():
+    color = input("What is your favorite colour? ")
+
+    if color == "red" or color == "RED" or color == "Red":
+        print("I like red too")
+    else:
+        print("I don't like", color)
+
+
+def chlg16():
+    answer = input("It is raining? ")
+
+    if answer.lower() == "yes":
+        answer = input("It is windy? ")
+
+        if answer.lower() == "yes":
+            print("It is too windy for an umbrella")
+        else:
+            print("Take an umbrella")
+
+    else:
+        print("Enjoy your day")
+
+
+def chlg17():
+    age = int(input("How old are you? "))
+
+    if age >= 18:
+        print("You can vote")
+    elif age == 17:
+        print("You can learn to drive")
+    elif age == 16:
+        print("You can buy a lottery ticket")
+    else:
+        print("You can go Trick-or-Treating")
+
+
+def chlg18():
+    num = int(input("Insert a number: "))
+
+    if num >= 10 and num <= 20:
+        print("Correct")
+    elif num < 10:
+        print("Too low")
+    else:
+        print("Too high")
+
+
+def chlg19():
+    num = int(input("Choose a option 1, 2 or 3: "))
+
+    if num == 1:
+        print("Thank you")
+    elif num == 2:
+        print("Well done")
+    elif num == 3:
+        print("Correct")
+    else:
+        print("Error message")
+
+
+chlg19()
