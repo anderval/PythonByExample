05ede4e4142dab8a7acafc8404acf8b0ced9c329
Anderval <andervalsp@outlook.com>
2022-11-23 16:32:42 -0300

    Third Chapter

--- Strings/main.py
+++ Strings/main.py
@@ -6,7 +6,7 @@ def chlg20():
 def chlg21():
     firstname = input("Enter your first name: ")
     name = input("Enter your last name: ")
-    full_name = name+" "+surname
+    full_name = name + " " + surname
     print(full_name, len(full_name))
 
 
@@ -15,20 +15,44 @@ def chlg22():
     surname = input("Enter your last name: ")
     name = name.title()
     surname = surname.title()
-    full_name = name+" "+surname
+    full_name = name + " " + surname
 
     print(full_name)
+
 
 def chlg23():
     nursery_rhyme = input("Enter a nursery rhyme: ")
     length = len(nursery_rhyme)
     start_num = int(input("Digit a starting number: "))
-    end_num = int(input("Digit a ending number: "))
+    end_num = int(input("Digit a end number: "))
     print(nursery_rhyme[start_num:end_num])
 
 
-def chlg
+def chlg24():
+    word = input("Enter a word: ")
+    word = word.upper()
+    print(word)
 
 
-chlg22()
+def chlg25():
+    name = input("Enter your first name: ")
+    length = len(name)
+    if length < 5:
+        surname = input("enter your last name: ")
+        full_name = name+surname
+        print(full_name.upper())
+    else:
+        print(name.lower())
 
+def chlg26():
+    word = input("Enter a word: ")
+    if word[0].lower() == "a" or word[0].lower() == "e" or word[0].lower() == "i" or word[0].lower() == "o" or word[0].lower() == "u":
+        word = word+"way"
+        print(word.lower())
+    else:
+        word = word[1:len(word)]+"ay"
+        print(word.lower())
+
+
+
+chlg26()
