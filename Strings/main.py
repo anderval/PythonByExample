b53110fcdc9004d3797b57c51a4cab9ebf70f926
Anderval <andervalsp@outlook.com>
2022-11-23 14:36:42 -0300

    Third Chapter

--- Strings/main.py
+++ Strings/main.py
@@ -3,7 +3,32 @@ def chlg20():
     print(len(name))
 
 
+def chlg21():
+    firstname = input("Enter your first name: ")
+    name = input("Enter your last name: ")
+    full_name = name+" "+surname
+    print(full_name, len(full_name))
 
 
+def chlg22():
+    name = input("Enter your first name: ")
+    surname = input("Enter your last name: ")
+    name = name.title()
+    surname = surname.title()
+    full_name = name+" "+surname
 
-chlg20()
+    print(full_name)
+
+def chlg23():
+    nursery_rhyme = input("Enter a nursery rhyme: ")
+    length = len(nursery_rhyme)
+    start_num = int(input("Digit a starting number: "))
+    end_num = int(input("Digit a ending number: "))
+    print(nursery_rhyme[start_num:end_num])
+
+
+def chlg
+
+
+chlg22()
+
